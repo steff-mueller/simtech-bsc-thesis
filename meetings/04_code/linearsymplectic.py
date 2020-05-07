ce49dec460495e7cf9576256678c51d01f973262
Steffen Mueller <arxisos@gmail.com>
2020-05-07 12:02:59 +0200

    start implementing SympNet

---
+++ meetings/04_code/linearsymplectic.py
@@ -0,0 +1,38 @@
+import torch
+from torch import nn
+
+# l_up for now
+class LinearSymplectic(nn.Module):
+    def __init__(self, d, h, bias=True):
+        super(LinearSymplectic, self).__init__()
+        self.d = d
+        self.h = h
+
+        self.S = nn.Parameter(torch.Tensor(d, d))
+        if bias:
+            self.bias = nn.Parameter(torch.Tensor(2*d))
+        else:
+            self.register_parameter('bias', None)
+
+    # from https://github.com/pytorch/pytorch/blob/master/torch/nn/modules/linear.py#L79
+    def reset_parameters(self):
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if self.bias is not None:
+            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+            bound = 1 / math.sqrt(fan_in)
+            nn.init.uniform_(self.bias, -bound, bound)
+
+    def forward(self, input):
+        symmetric_matrix = (self.S + self.S.t())/2
+
+        x_top = input[1:d]
+        x_bottom = input[d+1:2*d]
+
+        result_top = x_top + symmetric_matrix.mm(x_bottom)
+        result_bottom = x_bottom
+
+        return result_top.cat(result_bottom) + self.bias
+
+
+
+
