66d0dd74ceabba3c55884ceb44a34adde64b3d49
Steffen Mueller <arxisos@gmail.com>
2020-05-21 13:59:21 +0200

    implement lower linear symplecticc unit

--- meetings/04_code/linearsymplectic.py
+++ meetings/04_code/linearsymplectic.py
@@ -1,10 +1,9 @@
 import torch
 from torch import nn
 
-# l_up for now
-class LinearSymplectic(nn.Module):
+class UpperLinearSymplectic(nn.Module):
     def __init__(self, d, h, bias=True):
-        super(LinearSymplectic, self).__init__()
+        super(UpperLinearSymplectic, self).__init__()
         self.d = d
         self.h = h
 
@@ -15,10 +14,11 @@ class LinearSymplectic(nn.Module):
             self.register_parameter('bias', None)
 
     # from https://github.com/pytorch/pytorch/blob/master/torch/nn/modules/linear.py#L79
+    # TODO check if this initialization strategy makes sense in this application
     def reset_parameters(self):
         nn.init.kaiming_uniform_(self.S, a=math.sqrt(5))
         if self.bias is not None:
-            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.S)
             bound = 1 / math.sqrt(fan_in)
             nn.init.uniform_(self.bias, -bound, bound)
 
@@ -28,7 +28,20 @@ class LinearSymplectic(nn.Module):
         x_top = input[0:self.d]
         x_bottom = input[self.d:2*self.d]
 
-        result_top = x_top + symmetric_matrix.mm(x_bottom)
-        result_bottom = x_bottom
+        result_top = self._matrix_calc_top(symmetric_matrix, x_top, x_bottom)
+        result_bottom = self._matrix_calc_bottom(symmetric_matrix, x_top, x_bottom)
 
         return torch.cat([result_top, result_bottom]) + self.bias
+
+    def _matrix_calc_top(self, symmetric_matrix, x_top, x_bottom):
+        return x_top + symmetric_matrix.mm(x_bottom)
+    
+    def _matrix_calc_bottom(self, symmetric_matrix, x_top, x_bottom):
+        return x_bottom
+
+class LowerLinearSymplectic(UpperLinearSymplectic):
+   def _matrix_calc_top(self, symmetric_matrix, x_top, x_bottom):
+       return x_top
+   
+   def _matrix_calc_bottom(self, symmetric_matrix, x_top, x_bottom):
+       return symmetric_matrix.mm(x_top) + x_bottom
--- meetings/04_code/test_linearsymplectic.py
+++ meetings/04_code/test_linearsymplectic.py
@@ -1,10 +1,10 @@
 import unittest
 import torch
-from linearsymplectic import LinearSymplectic
+from linearsymplectic import UpperLinearSymplectic, LowerLinearSymplectic
 
 class TestLinearSympletic(unittest.TestCase):
-    def test_calculation(self):
-        module = LinearSymplectic(d=2, h=0.1)
+    def test_upper(self):
+        module = UpperLinearSymplectic(d=2, h=0.1)
 
         matrix = torch.tensor(
             [[1, 0, 2, 3],
@@ -33,12 +33,41 @@ class TestLinearSympletic(unittest.TestCase):
 
         expected = matrix.mm(input) + bias
         actual = module.forward(input)
-
-        print(expected)
-        print(actual)
         
         torch.testing.assert_allclose(actual, expected)
 
+    def test_lower(self):
+        module = LowerLinearSymplectic(d=2, h=0.1)
+
+        matrix = torch.tensor(
+            [[1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [2, 3, 1, 0],
+            [3, 1, 0, 1]],
+            dtype=torch.float
+        )
+
+        input = torch.ones(4,1)
+
+        bias = torch.tensor(
+            [[1],
+            [3],
+            [6],
+            [-3]],
+            dtype=torch.float
+        )
+
+        module.S.data = torch.tensor(
+            [[2,3],
+            [3,1]]
+        )
+
+        module.bias.data = bias
+
+        expected = matrix.mm(input) + bias
+        actual = module.forward(input)
+        
+        torch.testing.assert_allclose(actual, expected)
 
 if __name__ == '__main__':
     unittest.main()
