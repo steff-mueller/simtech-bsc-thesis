ae6b3fad0c7b97046bbd989191e02f04d0ae2892
Steffen Mueller <arxisos@gmail.com>
2020-05-22 22:45:48 +0200

    implement nonlinear symplectic unit

---
+++ meetings/04_code/nonlinearsymplectic.py
@@ -0,0 +1,16 @@
+from linearsymplectic import UpperLinearSymplectic
+from torch import sigmoid
+
+class UpperNonlinearSymplectic(UpperLinearSymplectic):
+    def _matrix_calc_top(self, symmetric_matrix, x_top, x_bottom):
+        return x_top + sigmoid(x_bottom)
+    
+    def _matrix_calc_bottom(self, symmetric_matrix, x_top, x_bottom):
+        return x_bottom
+
+class LowerNonlinearSymplectic(UpperLinearSymplectic):
+    def _matrix_calc_top(self, symmetric_matrix, x_top, x_bottom):
+        return x_top
+   
+    def _matrix_calc_bottom(self, symmetric_matrix, x_top, x_bottom):
+        return sigmoid(x_top) + x_bottom
---
+++ meetings/04_code/test_nonlinearsympletic.py
@@ -0,0 +1,62 @@
+import unittest
+import torch
+from torch import sigmoid
+from nonlinearsymplectic import UpperNonlinearSymplectic, LowerNonlinearSymplectic
+
+class TestNonlinearSymplectic(unittest.TestCase):
+    def test_upper(self):
+        module = UpperNonlinearSymplectic(d=2, h=0.1)
+
+        p = torch.ones(2,1)
+        q = torch.ones(2,1)
+        input = torch.cat([p, q])
+
+        bias = torch.tensor(
+            [[1],
+            [3],
+            [6],
+            [-3]],
+            dtype=torch.float
+        )
+
+        module.S.data = torch.tensor(
+            [[2,3],
+            [3,1]]
+        )
+
+        module.bias.data = bias
+
+        expected = torch.cat([p + sigmoid(q), q]) + bias
+        actual = module.forward(input)
+        
+        torch.testing.assert_allclose(actual, expected)
+    
+    def test_lower(self):
+        module = LowerNonlinearSymplectic(d=2, h=0.1)
+
+        p = torch.ones(2,1)
+        q = torch.ones(2,1)
+        input = torch.cat([p, q])
+
+        bias = torch.tensor(
+            [[1],
+            [3],
+            [6],
+            [-3]],
+            dtype=torch.float
+        )
+
+        module.S.data = torch.tensor(
+            [[2,3],
+            [3,1]]
+        )
+
+        module.bias.data = bias
+
+        expected = torch.cat([p, sigmoid(p) + q]) + bias
+        actual = module.forward(input)
+        
+        torch.testing.assert_allclose(actual, expected)
+
+if __name__ == '__main__':
+    unittest.main()
