cd27efc9a170985bcedc046c44e7ed19d11f2033
Steffen Mueller <arxisos@gmail.com>
2020-08-29 09:34:12 +0000

    - Fix tests for nonlinearsymplectic - Remove HarmonicUnit

--- nn/nonlinearsymplectic.py
+++ nn/nonlinearsymplectic.py
@@ -26,30 +26,3 @@ class LowerNonlinearSymplectic(UpperNonlinearSymplectic):
    
     def _matrix_calc_bottom(self, x_top, x_bottom):
         return self.a*self.activation_fn(x_top) + x_bottom
-
-# TODO only supports 2 dimensions at the moment.
-class HarmonicUnit(nn.Module):
-    def __init__(self, h):
-        super(HarmonicUnit, self).__init__()
-        self.h = h
-
-        self.dt = nn.Parameter(torch.Tensor(1))
-        self.m = nn.Parameter(torch.Tensor(1))
-        self.omega = nn.Parameter(torch.Tensor(1))
-
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        with torch.no_grad():
-            nn.init.uniform_(self.dt, -self.h, self.h)
-            nn.init.uniform_(self.m, 0.1, 1)
-            nn.init.uniform_(self.omega, 0.1, 1)
-
-    def forward(self, input):
-        q0 = input[:,0]
-        p0 = input[:,1]
-
-        q = q0*torch.cos(self.omega*self.dt) + p0/(self.m*self.omega)*torch.sin(self.omega*self.dt)
-        p = -self.m*self.omega*q0*torch.sin(self.omega*self.dt) + p0*torch.cos(self.omega*self.dt)
-
-        return torch.stack([q,p], dim=1)
--- tests/test_nonlinearsympletic.py
+++ tests/test_nonlinearsympletic.py
@@ -7,7 +7,8 @@ from nn.symplecticloss import symplectic_mse_loss
 
 class TestNonlinearSymplectic(unittest.TestCase):
     def test_upper(self):
-        module = UpperNonlinearSymplectic(dim=4)
+        module = UpperNonlinearSymplectic(dim=4, bias=True)
+        module.a.data = torch.ones(2)
 
         p = torch.ones(2,1)
         q = torch.ones(2,1)
@@ -29,7 +30,8 @@ class TestNonlinearSymplectic(unittest.TestCase):
         torch.testing.assert_allclose(torch.reshape(actual, (4,1)), expected)
     
     def test_lower(self):
-        module = LowerNonlinearSymplectic(dim=4)
+        module = LowerNonlinearSymplectic(dim=4, bias=True)
+        module.a.data = torch.ones(2)
 
         p = torch.ones(2,1)
         q = torch.ones(2,1)
@@ -49,28 +51,6 @@ class TestNonlinearSymplectic(unittest.TestCase):
         actual = module.forward(torch.reshape(input, (1,4)))
         
         torch.testing.assert_allclose(torch.reshape(actual, (4,1)), expected)
-
-    def test_harmonic(self):
-        h = 0.1
-        module = HarmonicUnit(h)
-
-        module.dt.data = torch.tensor(0.1)
-        module.omega.data = torch.tensor(math.pi/h)
-        module.m.data = torch.tensor(1.)
-
-        input = torch.tensor([
-            [0., 0.],
-            [1., 2.]
-        ], requires_grad=True)
-
-        actual = module.forward(input)
-        expected = torch.tensor([
-            [0., 0.],
-            [-1, -2]
-        ])
-
-        torch.testing.assert_allclose(symplectic_mse_loss(input, actual), 0)
-        torch.testing.assert_allclose(actual, expected)     
-     
+   
 if __name__ == '__main__':
     unittest.main()
