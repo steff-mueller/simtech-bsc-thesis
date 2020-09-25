a2dbd9c013868c06ba50e3212770054d1b85d957
Steffen Mueller <arxisos@gmail.com>
2020-09-25 09:50:58 +0000

    First implementation of Normalized Gradient Layer

--- nn/nonlinearsymplectic.py
+++ nn/nonlinearsymplectic.py
@@ -1,6 +1,7 @@
 from nn.linearsymplectic import SymplecticTriangularUnit
 import torch
 from torch import nn
+import math
 
 class UpperNonlinearSymplectic(SymplecticTriangularUnit):
     def __init__(self, dim, bias=False, activation_fn = torch.sigmoid, scalar_weight = False):
@@ -39,9 +40,11 @@ class UpperGradientModule(SymplecticTriangularUnit):
     def reset_parameters(self):
         super().reset_parameters()
         with torch.no_grad():
-            nn.init.constant_(self.a, 1)
-            nn.init.constant_(self.b, 0)
-            nn.init.eye_(self.K)
+            # Inspired by initialization of Linear layers:
+            # https://pytorch.org/docs/stable/generated/torch.nn.Linear.html
+            nn.init.uniform_(self.a, -math.sqrt(1/self.dim_half), math.sqrt(1/self.dim_half))
+            nn.init.uniform_(self.b, -math.sqrt(1/self.dim_half), math.sqrt(1/self.dim_half))
+            nn.init.uniform_(self.K, -math.sqrt(1/self.dim_half), math.sqrt(1/self.dim_half))
             
     def _matrix_calc_top(self, x_top, x_bottom):
         return x_top + (self.activation_fn(x_bottom.mm(self.K.t()) + self.b)*self.a).mm(self.K)
@@ -55,3 +58,37 @@ class LowerGradientModule(UpperGradientModule):
 
     def _matrix_calc_bottom(self, x_top, x_bottom):
         return x_bottom + (self.activation_fn(x_top.mm(self.K.t()) + self.b)*self.a).mm(self.K)
+
+class NormalizedUpperGradientModule(UpperGradientModule):
+    def __init__(self, dim:int, n:int, bias=False, activation_fn = torch.sigmoid, affine=True):
+        super(NormalizedUpperGradientModule, self).__init__(dim, n, bias, activation_fn)
+        self.affine = affine
+        if affine:
+            self.gamma = nn.Parameter(torch.Tensor(self.dim_half))
+            self.beta = nn.Parameter(torch.Tensor(self.dim_half))
+        else:
+            self.register_parameter('gamma', None)
+            self.register_parameter('beta', None)
+
+        self.register_buffer('var', torch.ones(self.dim_half))
+        self.register_buffer('mean', torch.zeros(self.dim_half))
+        self.reset_parameters2()
+
+    # TODO refactor
+    def reset_parameters2(self):
+        if self.affine:
+            with torch.no_grad():
+                nn.init.ones_(self.gamma)
+                nn.init.zeros_(self.beta)
+
+    def _matrix_calc_top(self, x_top, x_bottom):
+        if self.training:
+            self.var, self.mean = torch.var_mean(x_bottom, 0, unbiased=True)
+
+        eps = 1e-05
+        gamma = self.gamma if self.affine else 1
+        beta = self.beta if self.affine else 0
+        factor = gamma/torch.sqrt(self.var+eps)
+
+        x_bottom_normalized =  factor*(x_bottom - self.mean) + beta
+        return factor*super()._matrix_calc_top(x_top, x_bottom_normalized)
