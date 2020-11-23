ff82948a31b9fc7161974393f177b1eeba8eea13
Steffen Mueller <arxisos@gmail.com>
2020-11-23 21:57:43 +0000

    add plots for wave

--- scripts/thesis_wave.py
+++ scripts/thesis_wave.py
@@ -4,13 +4,15 @@ import asyncio
 import shutil
 import io
 import os
+import numpy as np
+import csv
 
 os.environ["PYTHONUNBUFFERED"] = "1"
 
 root_path = pathlib.Path(__file__).parent.absolute().joinpath('..')
 experiments_path = root_path.joinpath('experiments')
 data_path = root_path.joinpath('scripts/data-wave')
-latex_data_path = root_path.joinpath('latex/data')
+latex_data_path = root_path.joinpath('latex/data-wave')
 
 class Architecture:
     def __init__(self, name, extra_cmd=[]):
@@ -18,12 +20,14 @@ class Architecture:
         self.extra_cmd = extra_cmd
 
 class Experiment:
-    def __init__(self, name, cmd, activations, architectures, epochs):
+    def __init__(self, name, cmd, activations, architectures, epochs, l, n_x):
         self.name = name
         self.cmd = cmd
         self.activations = activations
         self.architectures = architectures
         self.epochs = epochs
+        self.l = l
+        self.n_x = n_x
 
 experiments = [
     Experiment('transport', 
@@ -37,6 +41,8 @@ experiments = [
             '--time-training', '1',
             '-lr', str(1e-1)
         ],
+        n_x = 100,
+        l = 1,
         activations=['sigmoid'],
         architectures=[
             Architecture('linear_canonical'),
@@ -56,6 +62,8 @@ experiments = [
             '--time-training', '1',
             '-lr', str(1e-2)
         ], 
+        n_x = 2000,
+        l = 50,
         activations=['sigmoid'], 
         architectures=[
             Architecture('nonlinear'),
@@ -74,6 +82,8 @@ experiments = [
             '-lr', str(1e-2),
             '--log-intermediate'
         ], 
+        n_x = 2000,
+        l = 50,
         activations=['sigmoid', 'tanh', 'elu'], 
         architectures=[
             Architecture('gradient'),
@@ -145,8 +155,66 @@ async def run_experiments(args):
     await asyncio.gather(*tasks)
 
 
-def update_csv():
-    pass
+def save_csv(dest, vec, header):
+    if not dest.parent.exists():
+        dest.parent.mkdir(parents=True)
+
+    np.savetxt(dest, vec, delimiter=',', header=header, comments='')
+
+def save_dict_as_csv(dest, dict_array):
+    with open(dest, 'w', newline='') as csvfile:
+        fieldnames = dict_array[0].keys()
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for dict in dict_array:
+            writer.writerow(dict)
+
+
+def subsample(vec: np.ndarray, every_nth: int):
+    """Subsamples rows"""
+
+    vec_sample = vec[::every_nth]
+    vec_sample[-1,:] = vec[-1,:] # return last
+    return vec_sample
+
+
+def print_q_over_domain(td_x: np.ndarray, t: float, exp: Experiment, dest: str):
+    x_t = td_x[np.isclose(td_x[:,0], t)]
+    d = int((x_t.shape[1] - 1)/2)
+    coord = np.linspace(-exp.l/2, exp.l/2, exp.n_x)
+    q = x_t[0,1:d+1]
+    save_csv(dest, np.stack((coord,q), axis=1), 'x,q')
+
+
+def update_csv(args):
+    for exp in experiments:
+        curr_exp_dir = data_path.joinpath(exp.name)
+        curr_destination_dir = latex_data_path.joinpath(exp.name)
+
+        td_x = np.load(curr_exp_dir
+            .joinpath(exp.architectures[0].name, exp.activations[0], 'exact_td_x.npy'))
+        
+        print_q_over_domain(td_x, 9.0, exp, curr_destination_dir.joinpath('exact', 'q_t9.csv'))
+
+        for arch in exp.architectures:
+            for activation in exp.activations:
+                # Training loss
+                losses = np.load(curr_exp_dir.joinpath(arch.name, activation, 'losses.npy'))
+                losses = np.stack([np.arange(0, len(losses)), losses], axis=1)
+                save_csv(curr_destination_dir
+                    .joinpath(arch.name, activation, 'loss.csv'), subsample(losses,5), header='epoch,loss')
+
+                # Test loss
+                losses = np.load(curr_exp_dir.joinpath(arch.name, activation, 'test_losses.npy'))
+                losses = np.stack([np.arange(0, len(losses)), losses], axis=1)
+                save_csv(curr_destination_dir
+                    .joinpath(arch.name, activation, 'test_loss.csv'), subsample(losses,5), header='epoch,loss')
+
+                td_x = np.load(curr_exp_dir
+                    .joinpath(arch.name, activation, 'epoch{}_td_x.npy'.format(exp.epochs)))
+
+                print_q_over_domain(td_x, 9.0, exp, curr_destination_dir.joinpath(arch.name, activation, 'q_t9.csv'))
 
 
 if __name__ == '__main__':
